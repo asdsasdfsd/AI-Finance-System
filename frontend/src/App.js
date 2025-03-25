3be3efbccbe723623f3b72ef524386e86c95a714
asdsasdfsd <102040474+asdsasdfsd@users.noreply.github.com>
2025-03-25 21:33:49 +0800

    feat: 完成登录界面和导航切换功能

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Button } from "antd";
+import AppRoutes from './routes/AppRoutes';
+
 export default function App() {
   return (
     <div>
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
+      {/*
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+    */}
+      <AppRoutes />
     </div>
   );
 }
