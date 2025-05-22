f40e3b733cf54a64f75bac98c43995589de8482f
SAIWA <zouding@foxmail.com>
2025-05-22 15:03:14 +0800

    Merge branch 'master' into ZDY_company_user_derpart_manage

--- backend/src/main/java/org/example/backend/repository/FundRepository.java
+++ backend/src/main/java/org/example/backend/repository/FundRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface FundRepository extends JpaRepository<Fund, Integer> {
+    List<Fund> findByCompanyCompanyId(Integer companyId);
     List<Fund> findByCompany(Company company);
     List<Fund> findByCompanyAndIsActive(Company company, Boolean isActive);
 }
